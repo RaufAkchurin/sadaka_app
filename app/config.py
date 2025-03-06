d69fbae914c48b433b872a889d2b42cbed673938
raufakchurin <abdurauf.dev@gmail.com>
2025-03-06 18:47:47 +0500

    конец дня

--- app/config.py
+++ app/config.py
@@ -4,14 +4,30 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     BASE_DIR: str = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-    DATABASE_URL: str = f"sqlite+aiosqlite:///{BASE_DIR}/data/db.sqlite3"
     SECRET_KEY: str
     ALGORITHM: str
+    TESTING: int
+    ENVIRONMENT: str
+
+    TEST_POSTGRES_PASSWORD: str
+    TEST_POSTGRES_USER: str
+    TEST_POSTGRES_DB: str
+    TEST_POSTGRES_HOST: str
+    TEST_POSTGRES_PORT: int
 
     model_config = SettingsConfigDict(env_file=f"{BASE_DIR}/.env")
 
-
-
 # Получаем параметры для загрузки переменных среды
 settings = Settings()
-database_url = settings.DATABASE_URL
+
+if settings.TESTING == 1:
+    database_url = (
+        f"postgresql+asyncpg://{settings.TEST_POSTGRES_USER}:{settings.TEST_POSTGRES_PASSWORD}@{settings.TEST_POSTGRES_HOST}:{settings.TEST_POSTGRES_PORT}/{settings.TEST_POSTGRES_DB}"
+    )
+else:
+    database_url = f"sqlite+aiosqlite:///{settings.BASE_DIR}/data/db.sqlite3"
+
+
+
+
+
--- app/dao/database.py
+++ app/dao/database.py
@@ -8,6 +8,8 @@ from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_
 from app.config import database_url
 
 
+
+
 engine = create_async_engine(url=database_url)
 async_session_maker = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 str_uniq = Annotated[str, mapped_column(unique=True, nullable=False)]
