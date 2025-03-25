6054a3f538b9d50265f7cdd7699691f47c1a233d
raufakchurin <abdurauf.dev@gmail.com>
2025-03-25 19:07:48 +0500

    разделил модели

--- app/users/models.py
+++ app/users/models.py
@@ -6,44 +6,6 @@ from app.dao.database import Base, str_uniq
 
 
 @dataclass
-class Country(Base):
-    name: Mapped[str_uniq]
-    language: Mapped[str_uniq]
-    regions: Mapped[list["Region"]] = relationship(back_populates="region")
-
-    def __repr__(self):
-        return f"{self.__class__.__name__}(id={self.id}, name={self.name})"
-
-
-@dataclass
-class Region(Base):
-    name: Mapped[str_uniq]
-    logo: Mapped[str]
-    citys: Mapped[list["City"]] = relationship(back_populates="city")
-
-    country_id: Mapped[int] = mapped_column(ForeignKey('countrys.id'), nullable=True)
-    country: Mapped["Country"] = relationship("Country", back_populates="countrys", lazy="joined")
-
-
-
-    def __repr__(self):
-        return f"{self.__class__.__name__}(id={self.id}, name={self.name})"
-
-
-@dataclass
-class City(Base):
-    name: Mapped[str_uniq]
-    users: Mapped[list["User"]] = relationship(back_populates="user")
-
-    region_id: Mapped[int] = mapped_column(ForeignKey('citys.id'), nullable=True)
-    region: Mapped["Region"] = relationship("Region", back_populates="citys", lazy="joined")
-
-
-    def __repr__(self):
-        return f"{self.__class__.__name__}(id={self.id}, name={self.name})"
-
-
-@dataclass
 class Role(Base):
     name: Mapped[str_uniq]
     users: Mapped[list["User"]] = relationship(back_populates="role")
