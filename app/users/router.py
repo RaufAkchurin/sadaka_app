2dc72e44d623631cd608112d7472e958daccdfb5
raufakchurin <abdurauf.dev@gmail.com>
2025-04-06 11:33:04 +0500

    USER update use case

--- app/users/router.py
+++ app/users/router.py
@@ -6,6 +6,7 @@ from app.dependencies.dao_dep import get_session_with_commit
 from app.users.dao import UsersDAO
 from app.users.models import User
 from app.users.schemas import SUserInfo, UserUpdateAPI, EmailModel, UserActiveModel
+from app.users.use_cases.update import UpdateUserUseCase
 
 router = APIRouter()
 
@@ -15,25 +16,15 @@ async def get_me(user_data: User = Depends(get_current_user)) -> SUserInfo:
 
 
 @router.put("/update/")
-async def update_user(user_data: UserUpdateAPI,
+async def update_user(update_data: UserUpdateAPI,
                       session: AsyncSession = Depends(get_session_with_commit),
                       user: User = Depends(get_current_user)
                       ) -> UserUpdateAPI:
-    await UsersDAO(session).update(filters=EmailModel(email=user.email),
-                                            values=UserUpdateAPI(
-                                                name=user_data.name,
-                                                picture=user_data.picture,
-                                                city_id=user_data.city_id,
-                                                email=user_data.email,
-                                            ))
 
-    updated_user = await UsersDAO(session).find_one_or_none_by_id(user.id)
-    return UserUpdateAPI(
-        name=updated_user.name,
-        picture=updated_user.picture,
-        city_id=updated_user.city_id,
-        email=updated_user.email,
-    )
+    dao = UsersDAO(session)
+    use_case = UpdateUserUseCase(dao)
+    return await use_case.execute(user=user, update_data=update_data)
+
 
 @router.delete("/delete/")
 async def update_user(session: AsyncSession = Depends(get_session_with_commit),
---
+++ app/users/use_cases/update.py
@@ -0,0 +1,25 @@
+from app.users.dao import UsersDAO
+from app.users.models import User
+from app.users.schemas import UserUpdateAPI, EmailModel
+
+
+class UpdateUserUseCase:
+    def __init__(self, users_dao: UsersDAO):
+        self.users_dao = users_dao
+
+    async def execute(self, user: User, update_data: UserUpdateAPI) -> UserUpdateAPI:
+        await self.users_dao.update(filters=EmailModel(email=user.email),
+                                       values=UserUpdateAPI(
+                                           name=update_data.name,
+                                           picture=update_data.picture,
+                                           city_id=update_data.city_id,
+                                           email=update_data.email,
+                                       ))
+
+        updated_user = await self.users_dao.find_one_or_none_by_id(data_id=user.id)
+        return UserUpdateAPI(
+                                name=updated_user.name,
+                                picture=updated_user.picture,
+                                city_id=updated_user.city_id,
+                                email=updated_user.email,
+                            )
