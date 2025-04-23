428c6e48d438cf97e813f4ffdcfce9dd267ea5d7
raufakchurin <abdurauf.dev@gmail.com>
2025-04-23 18:41:06 +0500

    preview added

--- admin/views/all.py
+++ admin/views/all.py
@@ -1,93 +1,73 @@
-from sqladmin import ModelView
-
+from admin.views.base import BaseAdminView, CreateWithPictureAdmin
 from app.documents.models import Document
 from app.fund import City, Country, Region
 from app.fund.models import Fund
 from app.payments.models import Payment
 from app.project.models import Project, Stage
-from app.users.models import Role, User
+from app.users.models import Role
+
+############### ПОЛЬЗОВАТЕЛИ ###############  # noqa E266
 
 
-class BaseView(ModelView):
-    form_excluded_columns = [User.created_at, User.updated_at]
-
-
-############### ПОЛЬЗОВАТЕЛИ ###############
-
-
-class RoleAdmin(BaseView, model=Role):
-    column_list = [Role.id, Role.name]
+class RoleAdmin(BaseAdminView, model=Role):
     icon = "fa-solid fa-user-tag"
     name = "Роль"
     name_plural = "Роли"
 
 
-# class UserAdmin(BaseView, model=User):
-#     column_list = [User.id, User.name]
-#     icon = "fa-solid fa-user"
-#     name = "Пользователь"
-#     name_plural = "Пользователи"
+############### ГЕОГРАФИЯ ############### # noqa E266
 
 
-############### ГЕОГРАФИЯ ###############
-
-
-class CityAdmin(BaseView, model=City):
-    column_list = [City.id, City.name]
+class CityAdmin(BaseAdminView, model=City):
     icon = "fa-solid fa-city"
     name = "Город"
     name_plural = "Города"
 
 
-class RegionAdmin(BaseView, model=Region):
-    column_list = [Region.id, Region.name]
+class RegionAdmin(CreateWithPictureAdmin, model=Region):
     icon = "fa-solid fa-map"
     name = "Регион"
     name_plural = "Регионы"
 
 
-class CountryAdmin(BaseView, model=Country):
-    column_list = [Country.id, Country.name]
+class CountryAdmin(BaseAdminView, model=Country):
     icon = "fa-solid fa-globe"
     name = "Страна"
     name_plural = "Страны"
 
 
-############### ФОНДЫ И ПРОЕКТЫ ###############
+############### ФОНДЫ И ПРОЕКТЫ ############### # noqa E266
 
 
-class FundAdmin(BaseView, model=Fund):
-    column_list = [Fund.id, Fund.name]
+class FundAdmin(CreateWithPictureAdmin, model=Fund):
     icon = "fa-solid fa-hand-holding-heart"
     name = "Фонд"
     name_plural = "Фонды"
 
 
-class ProjectAdmin(BaseView, model=Project):
-    column_list = [Project.id, Project.name]
+class ProjectAdmin(CreateWithPictureAdmin, model=Project):
     icon = "fa-solid fa-diagram-project"
     name = "Проект"
     name_plural = "Проекты"
 
 
-class StageAdmin(BaseView, model=Stage):
+class StageAdmin(BaseAdminView, model=Stage):
     column_list = [Stage.id, Stage.name]
     icon = "fa-solid fa-layer-group"
     name = "Этап"
     name_plural = "Этапы"
 
 
-class DocumentAdmin(BaseView, model=Document):
-    column_list = [Document.id, Document.name]
+class DocumentAdmin(BaseAdminView, model=Document):
     icon = "fa-solid fa-file-alt"
     name = "Документ"
     name_plural = "Документы"
 
 
-############### ПЛАТЕЖИ ###############
+############### ПЛАТЕЖИ ############### # noqa E266
 
 
-class PaymentAdmin(BaseView, model=Payment):
+class PaymentAdmin(BaseAdminView, model=Payment):
     column_list = [Payment.id]
     icon = "fa-solid fa-credit-card"
     name = "Платёж"
--- admin/views/user.py
+++ admin/views/user.py
@@ -1,12 +1,8 @@
-from fastapi import Request, UploadFile
-from sqladmin import ModelView
-from wtforms import FileField
-
-from app.s3_storage.use_cases.s3_upload import UploadFileUseCase
+from admin.views.base import CreateWithPictureAdmin
 from app.users.models import User
 
 
-class UserAdmin(ModelView, model=User):
+class UserAdmin(CreateWithPictureAdmin, model=User):
     name_plural = "Пользователи"
     name = "Пользователь"
 
@@ -18,8 +14,6 @@ class UserAdmin(ModelView, model=User):
         User.language,
         User.city_id,
         User.role_id,
-        User.picture_url,
-        # "avatar_preview"
     ]
     column_searchable_list = [User.name, User.email]
     column_sortable_list = [User.id, User.name, User.email]
@@ -28,72 +22,4 @@ class UserAdmin(ModelView, model=User):
         User.name: "Имя",
         User.email: "Email",
         User.language: "Язык",
-        User.picture_url: "URL аватарки",
-        # "avatar_preview": "Аватар",
     }
-
-    form_columns = [
-        User.name,
-        User.email,
-        User.language,
-        User.city_id,
-        User.role_id,
-        # "avatar_preview",
-    ]
-
-    async def scaffold_form(self, form_rules=None):
-        form_class = await super().scaffold_form()
-        form_class.picture_file = FileField("Загрузить аватар")
-        return form_class
-
-    async def insert_model(
-        self,
-        request: Request,
-        data: dict,
-    ):
-        form = await request.form()
-        file: UploadFile = form.get("picture_file")
-
-        if file and file.filename:
-            use_case = UploadFileUseCase()
-            s3_path = await use_case(file=file)
-            data["picture_url"] = s3_path
-
-        return await super().insert_model(request, data)
-
-    #
-
-    # async def update_model(self, request: Request, pk: str, data: dict) -> Any:
-    #     return await super().update_model(request=request, pk=pk, data=data)
-
-    # async def update_model(self,
-    #                        request: Request,
-    #                        data: dict,
-    #                        model: User,
-    #                        session: AsyncSession = Depends(get_session_with_commit),
-    #                        ):
-    #     form = await request.form()
-    #     file: UploadFile = form.get("picture_url")
-    #
-    #     if file and file.filename:
-    #         dao = UsersDAO(session)
-    #         use_case = UserLogoUpdateUseCase(users_dao=dao)
-    #         url = await use_case(user=model, picture=file)
-    #         data["picture_url"] = url
-    #
-    #     return await super().update_model(request=request, pk= data=data)
-
-    # def avatar_preview(self: User, value):
-    #     if self.picture_url:
-    #         return f'<img src="{self.picture_url}" width="50" height="50" style="object-fit:cover;border-radius:50%;" />'
-    #     return "—"
-
-    # column_formatters = {
-    #     "avatar_preview": avatar_preview
-    # }
-
-    # column_formatters_detail = column_formatters
-    # column_formatters_list = column_formatters
-    # column_formatters_args = {
-    #     "avatar_preview": {"is_safe": True}
-    # }
