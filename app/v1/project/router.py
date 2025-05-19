158e678658d745caa070590e72870fe9fb09ff26
raufakchurin <abdurauf.dev@gmail.com>
2025-05-19 15:59:09 +0500

    custom pagination without DB

--- app/v1/project/router.py
+++ app/v1/project/router.py
@@ -1,5 +1,4 @@
 from fastapi import APIRouter, Depends, Query
-from pydantic import BaseModel, conint
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.exceptions import ProjectNotFoundException
@@ -8,20 +7,16 @@ from app.v1.dependencies.auth_dep import get_current_user
 from app.v1.dependencies.dao_dep import get_session_with_commit
 from app.v1.project.enums import AbstractStatusEnum
 from app.v1.project.schemas import ProjectDetailAPISchema, ProjectForListAPISchema
-from app.v1.project.use_cases.list import ProjectListUseCase
+from app.v1.project.use_cases.list import PaginationParams, Paginator, PaginatorResponseSchema, ProjectListUseCase
 from app.v1.users.dao import ProjectDAO
 
 v1_projects_router = APIRouter()
 
 
 # TODO for tests add with status all
-# TODO add test for paginator func
-class PaginationParams(BaseModel):
-    page: conint(gt=0) = Query(1, description="Page number")
-    limit: conint(gt=0) = Query(25, description="Number of items per page")
 
 
-@v1_projects_router.get("/all/{status_of_project}", response_model=list[ProjectForListAPISchema])
+@v1_projects_router.get("/all/{status_of_project}", response_model=PaginatorResponseSchema)
 async def get_projects_list(
     status_of_project: AbstractStatusEnum,
     fund_id: int = Query(default=None, alias="fund_id"),
@@ -30,8 +25,8 @@ async def get_projects_list(
     session: AsyncSession = Depends(get_session_with_commit),
 ) -> list[ProjectForListAPISchema]:
     project_list_use_case = ProjectListUseCase()
-
-    return await project_list_use_case(status_of_project, fund_id, pagination.page, pagination.limit, session)
+    projects = await project_list_use_case(status_of_project, fund_id, session)
+    return await Paginator.pagination(projects, pagination.page, pagination.limit)
 
 
 @v1_projects_router.get("/detail/{project_id}", response_model=ProjectDetailAPISchema)
--- app/v1/project/use_cases/list.py
+++ app/v1/project/use_cases/list.py
@@ -1,9 +1,14 @@
+import math
+
+from fastapi import Query
+from pydantic import BaseModel, conint
+
 from app.v1.project.schemas import FundIdFilter, ProjectForListAPISchema, StatusFilter
 from app.v1.users.dao import ProjectDAO
 
 
 class ProjectListUseCase:
-    async def __call__(self, status_of_project, fund_id, page, limit, session) -> list[ProjectForListAPISchema]:
+    async def __call__(self, status_of_project, fund_id, session) -> list[ProjectForListAPISchema]:
         if status_of_project.value in ["active", "finished"]:
             if fund_id is None:
                 filtered_projects = await ProjectDAO(session=session).find_all(
@@ -20,13 +25,39 @@ class ProjectListUseCase:
             else:
                 filtered_projects = await ProjectDAO(session=session).find_all(filters=FundIdFilter(fund_id=fund_id))
 
-        filtered_projects = await pagination(filtered_projects, page, limit)
-
         return [ProjectForListAPISchema.model_validate(project) for project in filtered_projects]
 
 
-async def pagination(queryset: list, page: int, limit: int):
-    offset = (page - 1) * limit
-    paginated_queryset = queryset[offset : offset + limit]
+# TODO add test for paginator func
+class PaginationParams(BaseModel):
+    page: conint(gt=0) = Query(1, description="Page number")
+    limit: conint(gt=0) = Query(2, description="Number of items per page")
 
-    return paginated_queryset
+
+class PaginationResponseStateSchema(BaseModel):
+    page: int
+    size: int
+    total_pages: int
+    total_items: int
+
+
+class PaginatorResponseSchema(BaseModel):
+    items: list
+    state: PaginationResponseStateSchema
+
+
+class Paginator:
+    @staticmethod
+    async def pagination(queryset: list, page: int, limit: int):
+        offset = (page - 1) * limit
+        paginated_queryset = queryset[offset : offset + limit]
+
+        return PaginatorResponseSchema(
+            items=paginated_queryset,
+            state=PaginationResponseStateSchema(
+                page=page,
+                size=limit,
+                total_pages=math.ceil(len(queryset) / limit),
+                total_items=len(queryset),
+            ),
+        )
