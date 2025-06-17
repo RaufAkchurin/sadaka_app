0acccaadf1badfe05a65a29ed3a90547462b9144
raufakchurin <abdurauf.dev@gmail.com>
2025-06-17 10:49:37 +0500

    insert model not finished

--- app/admin/views/file.py
+++ app/admin/views/file.py
@@ -4,13 +4,18 @@ from models.file import File
 
 class FileAdmin(FileModelPictureUploadField, model=File):
     # TODO у файла поля отображаются только заполненные все сотальные скрывать
-    # TODO тк привязка напрмиер только к юзеру а все остальное ненужно видеть в таком случае
+    # TODO привязка напрмиер только к юзеру а все остальное ненужно видеть в таком случае
 
     icon = "fa-solid fa-file-alt"
     name = "Файл"
     name_plural = "Файлы"
     can_export = False
     form_excluded_columns = [
+        "name",  # because it inserts automatic
+        "size",  # because it inserts automatic
+        "mime",  # because it inserts automatic
+        "type",  # because it inserts automatic
+        "url",  # because it inserts automatic
         "created_at",
         "updated_at",
     ]
