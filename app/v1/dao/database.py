d7c9db6b7d6bec9e8037dd3453a967e295862097
raufakchurin <abdurauf.dev@gmail.com>
2025-06-23 11:32:25 +0500

    POSTGRESS prod data

--- app/v1/dao/database.py
+++ app/v1/dao/database.py
@@ -12,8 +12,11 @@ if settings.MODE == "TEST":
     DATABASE_URL = f"sqlite+aiosqlite:///{settings.BASE_DIR}/data/db_test.sqlite3"
     DATABASE_PARAMS = {"echo": True, "poolclass": NullPool}
 else:
-    DATABASE_URL = f"sqlite+aiosqlite:///{settings.BASE_DIR}/data/db.sqlite3"
-    DATABASE_PARAMS = {}
+    DATABASE_URL = (
+        f"postgresql+asyncpg://{settings.POSTGRES_USER}:"
+        f"{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOST}/{settings.POSTGRES_DB_NAME}"
+    )
+    DATABASE_PARAMS = {"pool_size": 5, "max_overflow": 10}
 
 engine = create_async_engine(url=DATABASE_URL, **DATABASE_PARAMS)
 async_session_maker = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
