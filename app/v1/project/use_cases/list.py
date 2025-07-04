622a33fe61be0f51e76ba35da205c9603f949c4b
ZaurBek <zaurbek.berzukaevv@gmail.com>
2025-07-05 01:50:33 +0300

    fix: add access control for projects and files
    - Add filtering for files in admin panel by fund access
    - Add filtering for projects in admin panel by fund access
    - Add access control in API routers for funds and projects
    - Add filtering in project list use case by fund access
    - Ensure users only see data from funds they have access to

--- app/v1/project/use_cases/list.py
+++ app/v1/project/use_cases/list.py
@@ -1,9 +1,10 @@
+from app.models.user import User
 from app.v1.project.schemas import FundIdFilter, ProjectForListAPISchema, StatusFilter
 from app.v1.users.dao import ProjectDAO
 
 
 class ProjectListUseCase:
-    async def __call__(self, status_of_project, fund_id, session) -> list[ProjectForListAPISchema]:
+    async def __call__(self, status_of_project, fund_id, session, user_data: User) -> list[ProjectForListAPISchema]:
         if status_of_project.value in ["active", "finished"]:
             if fund_id is None:
                 filtered_projects = await ProjectDAO(session=session).find_all(
@@ -19,5 +20,9 @@ class ProjectListUseCase:
                 filtered_projects = await ProjectDAO(session=session).find_all()
             else:
                 filtered_projects = await ProjectDAO(session=session).find_all(filters=FundIdFilter(fund_id=fund_id))
+
+        # Фильтруем проекты по доступу пользователя к фондам
+        if user_data.role.value != "superuser":
+            filtered_projects = [p for p in filtered_projects if p.fund_id in user_data.funds_access_ids]
 
         return [ProjectForListAPISchema.model_validate(project) for project in filtered_projects]
