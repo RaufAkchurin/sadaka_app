e8f120954dfe74739e48462305b3153bc2eef320
raufakchurin <abdurauf.dev@gmail.com>
2025-09-05 16:33:37 +0500

    payments

--- app/admin/views/payment.py
+++ app/admin/views/payment.py
@@ -8,9 +8,6 @@ class PaymentAdmin(FundAdminAccess, ModelView, model=Payment):
     icon = "fa-solid fa-credit-card"
     name = "Платёж"
     name_plural = "Платежи"
-    can_create = False
-    can_edit = False
-    can_delete = False
 
     column_exclude_list = ["id", "user", "captured_at", "updated_at", "project_id", "stage_id", "user_id"]
     column_searchable_list = [Payment.project_id]
