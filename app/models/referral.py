1d81f130b872a31200909ce321fb1afd9d3aa3d4
raufakchurin <abdurauf.dev@gmail.com>
2025-09-11 11:44:09 +0500

    model changes

--- app/models/referral.py
+++ app/models/referral.py
@@ -23,11 +23,18 @@ class Referral(Base):
     __tablename__ = "referrals"
 
     entity_type: Mapped[ReferralTypeEnum] = mapped_column(Enum(ReferralTypeEnum), nullable=False)
-    entity_id: Mapped[int] = mapped_column(nullable=True)
+
+    fund_id: Mapped[int] = mapped_column(ForeignKey("funds.id"), nullable=True)
+    fund: Mapped["Fund"] = relationship("Fund", back_populates="referrals", lazy="joined")  # noqa F821
+
+    project_id: Mapped[int] = mapped_column(ForeignKey("projects.id"), nullable=True)
+    project: Mapped["Project"] = relationship("Project", back_populates="referrals", lazy="joined")  # noqa F821
 
     key: Mapped[str] = mapped_column(String(6), unique=True, default=generate_short_key, nullable=False)
 
     sharer_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
-    sharer: Mapped["User"] = relationship("User", back_populates="referrals", lazy="joined")  # noqa
+    sharer: Mapped["User"] = relationship("User", back_populates="referrals", lazy="joined")  # noqa F821
 
-    payments: Mapped[list["Payment"]] = relationship("Payment", back_populates="referral")  # noqa F821
+    payments: Mapped[list["Payment"]] = relationship("Payment", back_populates="referral", lazy="joined")  # noqa F821
+
+    # если тай то соответствующий инстанс наличие проверяем и от
