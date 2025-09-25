1b883ecf31a20cff95e04f845a48c3ea5ca8114a
raufakchurin <abdurauf.dev@gmail.com>
2025-09-25 15:58:23 +0500

    admin panel payments

--- app/admin/views/payment.py
+++ app/admin/views/payment.py
@@ -9,5 +9,14 @@ class PaymentAdmin(FundAdminAccess, ModelView, model=Payment):
     name = "Платёж"
     name_plural = "Платежи"
 
+    column_list = [
+        Payment.id,
+        Payment.amount,
+        Payment.income_amount,
+        Payment.status,
+        Payment.project_id,
+        Payment.created_at,
+    ]
+
     column_exclude_list = ["id", "user", "captured_at", "updated_at", "project_id", "stage_id", "user_id"]
     column_searchable_list = [Payment.project_id]
